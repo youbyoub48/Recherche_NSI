63e8336fafdcc3285583cbac1ae4ecad3b78364c
youbyoub48 <ayou8@hotmail.fr>
2021-11-10 09:07:43 +0000

    c

--- src/Recherche/views.py
+++ src/Recherche/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+import API
+
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    if request.method == "GET":
+        return render(request, "index.html")
+    
+    elif request.method == "POST":
+        motif = request.POST.get("motif")
+        texte = request.POST.get("texte")
+        print(motif)
+        print(texte)
+        return render(request, "index.html")
