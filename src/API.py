1c3866834a24d60135bae1e6e154add60dc8f4cf
youbyoub48 <ayou8@hotmail.fr>
2021-11-10 08:42:06 +0000

    b

---
+++ src/API.py
@@ -0,0 +1,40 @@
+# Code Boyer-Moore-Horspool
+
+def tableau_sauts(motif):
+    tds={}
+    for i in range(len(motif)-2,-1,-1):
+        tds[motif[i]]=len(motif)-1-i
+    return tds
+        
+
+
+
+def boyer_moore_horspool(texte, motif,tDs):
+    size = len(texte)
+    taille = len(motif)
+    positions = []
+    if (taille<=size):
+        i=0
+        trouve=False
+        while (i<=size-taille):
+            for j in range (taille-1,-1,-1):
+                trouve=True
+                if(texte[i+j]!=motif[j]):
+                    if(texte[i+j] in tDs and tDs[texte[i+j]]<=j):
+                        i+=tDs[texte[i+j]]
+                    else:
+                        i+=j+1
+                    trouve=False
+                    break
+            if (trouve):
+                positions.append(i+1)
+                i += 1
+                trouve=False
+    return positions
+
+if __name__ == "__main__" :
+    texte = "ayoubbilaladam"
+    motif = "bilal"
+    tDs=tableau_sauts(motif)
+    azetr = boyer_moore_horspool(texte, motif, tDs)
+    
